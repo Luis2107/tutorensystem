574dbc472db3c49b961e8b89775f87eb769d9fd7
FinnHuelsbusch <f.huelsbusch@outlook.de>
2022-02-28 17:46:02 +0100

    added filter bar

--- frontend/src/components/overview/Overview.tsx
+++ frontend/src/components/overview/Overview.tsx
@@ -1,10 +1,42 @@
-import { Button, message } from 'antd';
+import { Button, Input, message, Select } from 'antd';
+import Form from 'antd/lib/form/Form';
+import FormItem from 'antd/lib/form/FormItem';
 import React from 'react';
 import { ping } from '../../api/api';
+
+
 
 const Overview: React.FC = () => {
   return (
     <div>
+      <FormItem
+        label={"Titel"}
+        name={"title"}>
+        <Input placeholder="Name, Beschreibung..." />
+      </FormItem>
+      <Form>
+        <FormItem
+          label={"Studiengang"}
+          name={"course"}>
+          <Select>
+            {[["winf","Wirtschaftsinformatik"], ["bwl","Betriebswirtschaftslehre"]].map(course => 
+              (<Select.Option value={course[0]}>{course[1]}</Select.Option>)
+              )
+            }
+          </Select>
+        </FormItem>
+        <FormItem
+          label={"Modul"}
+          name={"module"}>
+            <Select>
+              {[["mod1","Module1"], ["mod2","Module2"]].map(module => 
+                (<Select.Option value={module[0]}>{module[1]}</Select.Option>)
+                )
+              }
+          </Select>
+        </FormItem>
+
+      </Form>
       <Button onClick={e => {
         ping().then(
           res => message.success(res),
